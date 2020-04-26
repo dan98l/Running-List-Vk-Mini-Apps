b95d12b2f86e5a8b0229d063d89decc238affe49
Daniil <dan98l@mail.ru>
2020-04-26 14:39:27 +0700

    4 commit

---
+++ build/precache-manifest.5aedf57129be8ce041c379bb6829c8f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77daafc16441573afb9b3282ff13fa3d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "771349f32e3283ea477c",
+    "url": "./static/css/2.abebce0a.chunk.css"
+  },
+  {
+    "revision": "24b4c61d320e5c0be5b7",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "771349f32e3283ea477c",
+    "url": "./static/js/2.2c4b9ae3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2c4b9ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b4c61d320e5c0be5b7",
+    "url": "./static/js/main.a5dd3f45.chunk.js"
+  },
+  {
+    "revision": "2a93a38a68d527794359",
+    "url": "./static/js/runtime-main.6afb8ebc.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
